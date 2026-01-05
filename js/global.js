543ed059e306b6af9f7f0beb4957205791b1ad9f
Sabified <91898845+sabifiedsab@users.noreply.github.com>
2026-01-05 19:55:33 +0100

    start darkmode logic

--- js/global.js
+++ js/global.js
@@ -1,15 +1,10 @@
+const body = document.body;
 const button = document.getElementById("dark-mode-btn");
-function toggleDarkMode() {
-  if (button.textContent === "Toggle Light Mode") {
-    // Switch to light mode
-    console.log("Switching to light mode");
-    // TO DO: implement light mode functionality here
-    button.textContent = "Toggle Dark Mode";
-  } else {
-    // Switch to dark mode
-    console.log("Switching to dark mode");
-    // TO DO: implement dark mode functionality here
-    button.textContent = "Toggle Light Mode";
-  }
-}
-button.addEventListener("click", toggleDarkMode);
+
+button.addEventListener('click', () => {
+  const isDark = body.classList.toggle('darkmode');
+  localStorage.setItem('darkmode', isDark);
+});
+
+const saved = localStorage.getItem('darkmode') === 'true';
+body.classList.toggle('darkmode', saved);
